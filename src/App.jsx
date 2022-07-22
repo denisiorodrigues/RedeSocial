b2a486a6b857145cbfb7ff232754bf8d8d7a4f39
Denisio Rodrigues <denisio@ymail.com>
2022-07-22 01:23:53 -0300

    Componente sidebar parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post'
+
+import styles from './App.module.css';
 
 import './global.css'
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post 
-        author="Denisio Rodrigues"
-        content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex adipisci labore dolore perspiciatis! Quae, aliquid quas mollitia eum voluptate labore doloribus optio quidem, ipsa dolore velit, magnam nemo tempora."
-      />
-      <Post
-      author = "Guilherme Vilmar"
-      content= "Um bebê que gosta de açucar"
-      />
+      <Header />
+      <div className={styles.warapper} >
+        <Sidebar />
+        <main>
+          <Post
+            author="Denisio Rodrigues"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex adipisci labore dolore perspiciatis! Quae, aliquid quas mollitia eum voluptate labore doloribus optio quidem, ipsa dolore velit, magnam nemo tempora."
+          />
+          <Post
+            author="Guilherme Vilmar"
+            content="Um bebê que gosta de açucar"
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
