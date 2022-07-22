cf7ad656a8c9e32bc0b00617d1e6aa951684c02b
Denisio Rodrigues <denisio@ymail.com>
2022-07-21 21:20:50 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 
 export function App() {
   return (
     <div>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Header/>
+      <Post 
+        author="Denisio Rodrigues"
+        content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex adipisci labore dolore perspiciatis! Quae, aliquid quas mollitia eum voluptate labore doloribus optio quidem, ipsa dolore velit, magnam nemo tempora."
+      />
+      <Post
+      author = "Guilherme Vilmar"
+      content= "Um bebê que gosta de açucar"
+      />
     </div>
   )
 }
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+export function Header(){
+    console.log(styles)
+    return (
+        <header className={styles.header}>
+            <strong>Ignite Feed</strong>
+        </header>
+    );
+}
