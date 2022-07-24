e12b4dfdd91514ad1dddbdbbcfe635e12519b98f
Denisio Rodrigues <denisio@ymail.com>
2022-07-23 23:28:55 -0300

    Adicoinando a mensagem digitada no posto para o comentário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,11 @@ import styles from './Post.module.css'
 
 export function Post({author, publishedAt, content}) {
     
-    const [comments, setComments] = useState([1,2]);
+    const [comments, setComments] = useState([
+        'Post bem legal',    
+    ]);
+
+    const [newCommentText, setNewCommentText] = useState('');
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -19,9 +23,14 @@ export function Post({author, publishedAt, content}) {
         addSuffix: true,
     });
 
-    function handleCreateNewComment(){
+    function handleCreateNewComment() {
         event.preventDefault();
-        setComments([...comments, comments.length +1]);
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
     }
 
     return (
@@ -51,9 +60,14 @@ export function Post({author, publishedAt, content}) {
 
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
+
                 <textarea
+                    name="comment"
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
+
                 <footer>
                     <button type='submit'>Publicar</button>
                 </footer>
@@ -61,7 +75,9 @@ export function Post({author, publishedAt, content}) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment/>
+                    return <Comment 
+                                content={comment} 
+                            />
                 })}
             </div>
         </article>
