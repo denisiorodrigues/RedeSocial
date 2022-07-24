b927b5898da8193a6c20fd53a4a58435dc853eae
Denisio Rodrigues <denisio@ymail.com>
2022-07-23 22:01:06 -0300

    Trabalhando com datas do post

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const posts = [
       {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
       {type:'link', content:'👉 jane.design/doctorcare'}
     ],
-    publishedAt: new Date('2022-07-20 08:41:58')
+    publishedAt: new Date('2022-06-23 20:00'),
   },
   {
     id: 2,
@@ -33,7 +33,7 @@ const posts = [
       {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
       {type:'link', content:'👉 jane.design/doctorcare'}
     ],
-    publishedAt: new Date('2022-07-20 08:41:58')
+    publishedAt: new Date('2022-07-03 20:00'),
   }
 ];
 
